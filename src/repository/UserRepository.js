b52077a12ca1c5c3b5f463cbd0a9676348ce0050
webMasterAlumni <heraldofdevs@gmail.com>
2021-10-05 17:22:28 -0300

    intial commit

---
+++ src/repository/UserRepository.js
@@ -0,0 +1,10 @@
+const users = ['Lucas', 'Juarez', 'Daniel', 'baixinha']
+
+export default function getUsers() {
+    // return users
+    return []
+}
+
+export function addUsers(user) {
+    users.push(user)
+}
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import getUsers, { addUsers } from './repository/UserRepository.js'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    const users = getUsers()
+
+    return res.json({ users })
+})
+
+app.listen(3333, () => {
+    console.log('o servidor está rodando normalmente ...')
+})
