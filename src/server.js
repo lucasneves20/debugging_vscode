6ae8fea95906f5b9681bac9ea8ce9fd3e32a8a63
webMasterAlumni <heraldofdevs@gmail.com>
2021-10-05 18:09:11 -0300

    intial commit

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 import express from 'express'
-import getUsers, { addUsers } from './repository/UserRepository.js'
+import getUsers, {
+    addUsers
+} from './repository/UserRepository.js'
 
 const app = express()
 
 app.get('/', (req, res) => {
     const users = getUsers()
 
-    return res.json({ users })
+    return res.json({
+        users
+    })
 })
 
 app.listen(3333, () => {
     console.log('o servidor está rodando normalmente ...')
 })
-
-// Deus me ajude !!!!
